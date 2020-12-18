8b08a02cfd460562f88d70293f833b500d30e833
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2020-12-18 16:36:37 +0000

    Add files via upload

---
+++ Today.js
@@ -0,0 +1,66 @@
+//=============================================================================\\
+//                                  Today.JS                                    \\
+//                              made by nullcheats                               \\
+//================================================================================\\
+
+/*
+This will simply give us the "Date" "Month" "Year";
+We then split this by the "/" to be able to get individual parts
+*/
+let today = new Date().toLocaleDateString().split("/");
+const date = today[0];
+const month = today[1];
+const year = today[2];
+
+/*
+As the month returns a numberic value such as 12 being decemeber
+We make an array containing all possible months and cross check it
+For example if month = 4 then it will get the 4th value 'May'
+*/
+const Return_Month = () => {
+    var month_text = new Array(12);
+    month_text[0] = "January";
+    month_text[1] = "February";
+    month_text[2] = "March";
+    month_text[3] = "April";
+    month_text[4] = "May";
+    month_text[5] = "June";
+    month_text[6] = "June";
+    month_text[7] = "July";
+    month_text[8] = "August";
+    month_text[9] = "September";
+    month_text[10] = "October";
+    month_text[11] = "November";
+    month_text[12] = "December";
+    return month_text[month];
+}
+
+/*
+This is a simple function that returns the day from the "get.day"
+just like the month we use the number it gives us to cross check the array
+*/
+const return_Weekday = () => {
+    let day_info = new Date().getDay();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    return weekday[day_info];
+}
+
+
+/*
+This function is called via the Onload events
+This is where the values will be displayed to the user
+*/
+let LoadInfo = () => {
+    document.getElementById('Date_header').innerHTML = Return_Month();
+    document.getElementById('year').innerHTML = year;
+    document.getElementById('day').innerHTML = date;
+    document.getElementById('day_text').innerHTML = return_Weekday();
+    document.getElementById('All_Date').innerHTML = date + "/" + month + "/" + year;
+}
